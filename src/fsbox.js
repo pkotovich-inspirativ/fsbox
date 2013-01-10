5bd8ce6d7f2c96b6368b675fae3727029a15366b
evolchek <eugene.volchek@gmail.com>
2013-01-10 21:23:16 +0300

    core functionality

--- src/fsbox.js
+++ src/fsbox.js
@@ -1,28 +1,155 @@
-
-define([], function() {(function($) {
+(function($) {
+	
+	var ImgSlide = function($slide, $box) {
 		
+		$.extend(this, {
+			
+			nextSlide: null,
+			prevSlide: null,
+			$slide: $slide,
+			$box: $box,
+			loaded: false,
+			
+			setNextSlide: function(slide) {
+				
+				this.nextSlide = slide
+
+				$slide.click(function() { $box.fsbox('next') })
+			},
+			
+			setPrevSlide: function(slide) { this.prevSlide = slide },
+			
+			show: function() {
+				
+				var $slide = this.$slide, self = this
+				
+				if (this.loaded) $slide.fadeIn()
+				else this.load(function() { self.center(); $slide.fadeIn() })
+				
+				if (this.prevSlide) {
+					
+					this.prevSlide.load()
+					this.$box.data('fsbox').$prevControl.show()
+					
+				} else this.$box.data('fsbox').$prevControl.hide()
+				
+				if (this.nextSlide) {
+					
+					this.nextSlide.load()
+					this.$box.data('fsbox').$nextControl.show()
+					
+				} else this.$box.data('fsbox').$nextControl.hide()
+			},
+			
+			hide: function(callback) { this.$slide.fadeOut(400, callback) },
+			
+			load: function(callback) {
+				
+				this.$slide.load(callback)
+				
+				this.$slide.attr('src', this.$slide.data('fsboxSrc'))
+				this.loaded = true
+			},
+			
+			center: function() {
+				
+				$slide.css({
+					position: 'absolute',
+					top: ($slide.parent().height() - $slide.height())/2,
+					left: ($slide.parent().width() - $slide.width())/2
+				})
+			}
+		})
+	}
+	
 	var methods = {
 			
 		init: function(data) {
 			
-			if (!data) {
+			if (data) return
 				
-				this.data('fsbox', {})
+			var data = {}
+			
+			this.detach()
+				.css({position: 'absolute'})
+				.appendTo($('body'))
 				
-				this.detach()
-					.css({position: 'absolute'})
-					.appendTo($('body'))
+			var $box = this
+			
+			data.$prevControl = this.find('.prev')
+			data.$nextControl = this.find('.next')
+			
+			this.find('.close a').click(function() { $box.fsbox('close'); return false })
+			this.find('.prev a').click(function() { $box.fsbox('prev'); return false })
+			this.find('.next a').click(function() { $box.fsbox('next'); return false })
+
+			var prevSlide = null;
+			
+			data.slides = this.find('.slides > *').map(function() {
+
+				var $this = $(this), slide = new ImgSlide($this, $box)
+				
+				$this.hide()
+
+				if (prevSlide) {
 					
-				var box = this
+					prevSlide.setNextSlide(slide)
+					
+					slide.setPrevSlide(prevSlide)
+					
+				} else data.currentSlide = slide
 				
-				this.find('.close a').click(function() { box.hide() })
+				prevSlide = slide
 				
-				this.find('.slides > *').hide()
+				return slide
+				
+			}).get()
+			
+			this.data('fsbox', data)
+		},
+		
+		open: function() {
+			
+			var data = this.data('fsbox')
+			
+			data.bodyOverflow = $('body').css('overflow')
+			$('body').css('overflow', 'hidden')
+			
+			this.fadeIn()
+			
+			data.currentSlide.show()
+		},
+		
+		close: function() {
+			
+			this.fadeOut()
+			
+			$('body').css('overflow', this.data('fsbox').bodyOverflow)
+		},
+		
+		prev: function() {
+			
+			var currentSlide = this.data('fsbox').currentSlide
+			
+			if (currentSlide.prevSlide) {
+				
+				currentSlide.hide(function() { currentSlide.prevSlide.show() })
+				
+				this.data('fsbox').currentSlide = currentSlide.prevSlide
 			}
 		},
 		
-		show: function() { this.show() },
-		hide: function() { this.hide() },
+		next: function() {
+			
+			var currentSlide = this.data('fsbox').currentSlide
+			
+			if (currentSlide.nextSlide) {
+
+				currentSlide.hide(function() { currentSlide.nextSlide.show() })
+				
+				this.data('fsbox').currentSlide = currentSlide.nextSlide
+			}
+		}
 	}
 	
 	$.fn.fsbox = function(method) {
@@ -46,4 +173,4 @@ define([], function() {(function($) {
 	
 	$(document).ready(function() { $($.fn.fsbox.defaults.autoSelector).fsbox() })
 		
-}) (jQuery) })
+}) (jQuery)
